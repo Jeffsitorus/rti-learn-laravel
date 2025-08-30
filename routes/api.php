de9e30e3c75a2a9ddab7e9082bf1c5d5875f1571
Jeffsitorus <jefrisitorus22@gmail.com>
2025-08-30 10:52:53 +0700

    feat: crud categories

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 
@@ -10,5 +11,13 @@ Route::group(['namespace' => 'App\Http\Controllers'], function ($route) {
         $route->get('/all', [TagController::class, 'all']);
         $route->get('/{tag:id}', [TagController::class, 'show']);
         $route->patch('/{tag:id}', [TagController::class, 'update']);
+    });
+
+    $route->prefix('tags')->group(function ($route) {
+        $route->get('/', [CategoryController::class, 'index']);
+        $route->post('/', [CategoryController::class, 'store']);
+        $route->get('/all', [CategoryController::class, 'all']);
+        $route->get('/{tag:id}', [CategoryController::class, 'show']);
+        $route->patch('/{tag:id}', [CategoryController::class, 'update']);
     });
 });
